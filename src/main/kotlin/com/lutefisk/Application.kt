de469baec95d477bf5e9372f506064a233caac73
Per Kristian <per-kristian.svanberg@klaravik.se>
2024-04-22 15:27:54 +0200

    WIP - main application file

--- src/main/kotlin/com/lutefisk/Application.kt
+++ src/main/kotlin/com/lutefisk/Application.kt
@@ -1,13 +1,26 @@
 package com.lutefisk
 
+import com.lutefisk.plugins.configureSerialization
+import com.lutefisk.repository.UserRepository
+import com.lutefisk.routing.configureRouting
+import com.lutefisk.service.UserService
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 8080, host = "127.0.0.1", module = Application::module)
         .start(wait = true)
 }
 
 fun Application.module() {
+    configureSerialization()
+    configureRouting(
+        UserService(UserRepository())
+    )
 }
+
+/**
+ * TODO
+ * Encrypt password before persisting
+ */

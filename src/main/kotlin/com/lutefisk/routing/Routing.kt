a40c3b049a7df40f8c7da9e77c8b950b00460616
Per Kristian <per-kristian.svanberg@klaravik.se>
2024-04-26 10:29:42 +0200

    Updated main route file with authentication

--- src/main/kotlin/com/lutefisk/routing/Routing.kt
+++ src/main/kotlin/com/lutefisk/routing/Routing.kt
@@ -1,11 +1,16 @@
 package com.lutefisk.routing
 
+import com.lutefisk.service.JwtService
 import com.lutefisk.service.UserService
 import io.ktor.server.application.*
 import io.ktor.server.routing.*
 
-fun Application.configureRouting(userService: UserService) {
+fun Application.configureRouting(userService: UserService, jwtService: JwtService) {
     routing {
+        route("api/auth") {
+            authRoute(jwtService)
+        }
+
         route("/api/user") {
             userRoute(userService)
         }
